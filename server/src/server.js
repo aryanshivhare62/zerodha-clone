18137c9a85f1c2b4f849609c2a1785b600d17a11
aryanshivhare <aryanshivhare62@gmail.com>
2026-09-01 22:15:06 +0530

    database connected

---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const pool = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Zerodha Clone API is running");
+});
+
+app.get("/db-test", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows[0]);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
